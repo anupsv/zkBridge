2eecb00154fad5519bd7dcc64d2f562033201a99
asv <asv@fof.com>
2023-09-04 13:03:47 -0700

    adding poseidon check

--- hash-comparison/examples/halo2_lib.rs
+++ hash-comparison/examples/halo2_lib.rs
@@ -1,69 +1,48 @@
 use clap::Parser;
-use halo2_base::gates::{GateChip, GateInstructions};
-use halo2_base::utils::ScalarField;
-use halo2_base::AssignedValue;
 #[allow(unused_imports)]
 use halo2_base::{
-    Context,
     QuantumCell::{Constant, Existing, Witness},
 };
-use halo2_scaffold::scaffold::cmd::Cli;
-use halo2_scaffold::scaffold::run;
+use halo2_base::{gates::GateChip, utils::ScalarField, AssignedValue, Context};
+use halo2_scaffold::scaffold::{cmd::Cli, run};
+use poseidon::PoseidonChip;
 use serde::{Deserialize, Serialize};
+
+const T: usize = 2;
+const RATE: usize = 1;
+const R_F: usize = 8;
+const R_P: usize = 57;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct CircuitInput {
     pub x: String, // field element, but easier to deserialize as a string
+    pub keccak_hash: String, // field element, but easier to deserialize as a string
+    pub poseidon_hash: String, // field element, but easier to deserialize as a string
 }
 
 // this algorithm takes a public input x, computes x^2 + 72, and outputs the result as public output
 fn some_algorithm_in_zk<F: ScalarField>(
     ctx: &mut Context<F>,
-    input: CircuitInput,
+    inp: CircuitInput,
     make_public: &mut Vec<AssignedValue<F>>,
 ) {
-    let x = F::from_str_vartime(&input.x).expect("deserialize field element should not fail");
     // `Context` can roughly be thought of as a single-threaded execution trace of a program we want to ZK prove. We do some post-processing on `Context` to optimally divide the execution trace into multiple columns in a PLONKish arithmetization
     // More advanced usage with multi-threaded witness generation is possible, but we do not explain it here
 
-    // first we load a number `x` into as system, as a "witness"
-    let x = ctx.load_witness(x);
-    // by default, all numbers in the system are private
-    // we can make it public like so:
-    make_public.push(x);
+    // first we load a private input `x` (let's not worry about public inputs for now)
+    let hash_input = ctx.load_witness(F::from_str_vartime(&inp.x).unwrap());
+    let poseidon_hash_input = ctx.load_witness(F::from_str_vartime(&inp.poseidon_hash).unwrap());
+
+    make_public.extend([poseidon_hash_input]);
 
     // create a Gate chip that contains methods for basic arithmetic operations
     let gate = GateChip::<F>::default();
-
-    // ===== way 1 =====
-    // now we can perform arithmetic operations almost like a normal program using halo2-lib API functions
-    // square x
-    let x_sq = gate.mul(ctx, x, x);
-
-    // x^2 + 72
-    let c = F::from(72);
-    // the implicit type of most variables is an "Existing" assigned value
-    // a known constant is a separate type that we specify by `Constant(c)`:
-    let out = gate.add(ctx, x_sq, Constant(c));
-    // Halo2 does not distinguish between public inputs vs outputs because the verifier seems them all at the same time
-    // However in traditional terms, `out` is our output number. It is currently still private.
-    // Let's make it public:
-    make_public.push(out);
-    // ==== way 2 =======
-    // here is a more optimal way to compute x^2 + 72 using the lower level `assign_region` API:
-    let val = *x.value() * x.value() + c;
-    let _val_assigned =
-        ctx.assign_region_last([Constant(c), Existing(x), Existing(x), Witness(val)], [0]);
-    // the `[0]` tells us to turn on a vertical `a + b * c = d` gate at row position 0.
-    // this imposes the constraint c + x * x = val
-
-    // ==== way 3 ======
-    // this does the exact same thing as way 2, but with a pre-existing function
-    let _val_assigned = gate.mul_add(ctx, x, x, Constant(c));
-
-    println!("x: {:?}", x.value());
-    println!("val_assigned: {:?}", out.value());
-    assert_eq!(*x.value() * x.value() + c, *out.value());
+    let mut poseidon = PoseidonChip::<F, T, RATE>::new(ctx, R_F, R_P).unwrap();
+    poseidon.update(&[hash_input]);
+    let hash = poseidon.squeeze(ctx, &gate).unwrap();
+    make_public.push(hash);
+    assert_eq!(hash.value(), poseidon_hash_input.value());
+    println!("x: {:?}, poseidon(x): {:?}", poseidon_hash_input.value(), hash.value());
 }
 
 fn main() {
